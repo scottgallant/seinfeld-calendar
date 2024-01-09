51894f0f98fe0c107d1fdbb413d59f1fb9ffe64d
Scott Gallant <scottgallant@users.noreply.github.com>
2024-01-08 21:03:28 -0400

    Update specialDates.js

--- src/data/specialDates.js
+++ src/data/specialDates.js
@@ -3,6 +3,7 @@ const specialDates = [
     { "date": "2024-01-04", "description": "Bike 45mins" },
     { "date": "2024-01-05", "description": "Physio" },
     { "date": "2024-01-07", "description": "Strength Training" },
+    { "date": "2024-01-08", "description": "Strength Training" },
 ];
 
 export default specialDates;
