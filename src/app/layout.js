637290d0e64f55021cc4fc24216e9c55372763b1
Scott Gallant <scottgallant@gmail.com>
2024-01-11 15:29:40 -0400

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 // import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${GeistMono.className} `} >
       <body>{children}</body>
       <SpeedInsights />
+      <Analytics />
     </ html>
   )
 }
