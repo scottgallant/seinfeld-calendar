b928edeaef66fc2c4ea54bee02a1f71bbbb51a8b
Scott Gallant <scottgallant@gmail.com>
2024-01-12 10:28:29 -0400

    escaped quotes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="text-center text-xs text-gray-500 p-4">
-            <p className='mb-3'>Habit tracker inspired by <a href='https://jamesclear.com/stop-procrastinating-seinfeld-strategy' className="underline">The Seinfeld Strategy</a>. Log your daily habits (exercise, in this case). The goal is to <span className='italic'>"not break the chain"</span> of blue days.  </p>
+            <p className='mb-3'>Habit tracker inspired by <a href='https://jamesclear.com/stop-procrastinating-seinfeld-strategy' className="underline">The Seinfeld Strategy</a>. Log your daily habits (exercise, in this case). The goal is to <span className='italic'>&ldquo;not break the chain&rdquo;</span> of blue days.  </p>
             <p className='mb-3'>Read the <a href='https://scottgallant.net/posts/seinfeld-calendar/' className="underline">blog post</a> or fork it on <a href="https://github.com/scottgallant/seinfeld-calendar" className='underline'>GitHub</a>.</p>
         </footer>
     );
