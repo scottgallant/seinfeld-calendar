f9c23f0765d59d63d9d584dd6b5a81b94cc63114
Scott Gallant <scottgallant@gmail.com>
2024-01-20 23:33:57 -0400

    remove invert

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${GeistMono.className} invert`} >
+    <html lang="en" className={`${GeistMono.className} `} >
       <body>{children}</body>
       <SpeedInsights />
       <Analytics />
