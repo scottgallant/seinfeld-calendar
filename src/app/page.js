99969830f9c75b3d905f13fd3196500384e89c80
Scott Gallant <scottgallant@gmail.com>
2024-01-22 15:24:34 -0400

    updated meta tags

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,8 @@ const App = () => {
       <Head>
         <title>Seinfeld Calendar</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗓️</text></svg>" />
+        <meta name="description" content="A simple Next.js habit tracker app inspired by 'The Seinfeld Strategy'. Log your daily habits (exercise, in this example)."></meta>
+        
         {/* Open Graph */}
         <meta property="og:type" content="website" />
         <meta property="og:title" content="The Seinfeld Calendar - A Daily Habit Tracker " />
@@ -31,6 +32,8 @@ const App = () => {
         <meta name="twitter:image" content="https://seinfeld-calendar.vercel.app/seinfeld-calendar.png" />
         <meta name="twitter:image:width" content="1200" />
         <meta name="twitter:image:height" content="630" />
+
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗓️</text></svg>" />
       </Head>
       <Calendar specialDates={specialDates} />
       <Footer />
