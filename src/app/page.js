e1046fbdebf74f28481a0441f32e8c21a80adfa9
Scott Gallant <scottgallant@gmail.com>
2024-01-24 16:11:26 -0400

    updated dates

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ const App = () => {
         <title>Seinfeld Calendar</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
         <meta name="description" content="A simple Next.js habit tracker app inspired by 'The Seinfeld Strategy'. Log your daily habits (exercise, in this example)."></meta>
-        <meta name="og:url" content="https://seinfeld-calendar.vercel.app/"></meta>
+        {/* <meta name="og:url" content="https://seinfeld-calendar.vercel.app/"></meta> */}
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗓️</text></svg>" />
       </Head>
       <Calendar specialDates={specialDates} />
