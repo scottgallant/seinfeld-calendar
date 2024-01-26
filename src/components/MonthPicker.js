0a2d01b704fe676ceeebd5eaf97da0726b8d9690
Scott Gallant <scottgallant@gmail.com>
2024-01-26 11:20:15 -0400

    fixed drawer bug. autoformat code

--- src/components/MonthPicker.js
+++ src/components/MonthPicker.js
@@ -16,15 +16,15 @@ const MonthPicker = ({ selectedMonth, onMonthChange, monthNames, month }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-      <h2 className="text-center text-xl my-10">{monthNames[month]} <ChevronDown className="inline" /></h2>
+        <h2 className="text-center text-xl my-10">{monthNames[month]} <ChevronDown className="inline" /></h2>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
-      {months.map(month => (
-        <DropdownMenuItem key={month} onClick={() => onMonthChange(month)} className={`text-lg p-2 ${month === selectedMonth ? 'bg-blue-50' : ''
-      }`}> 
-        <span>{new Date(2024, month).toLocaleString('default', { month: 'long' })}</span>
-        </DropdownMenuItem>
-      ))}
+        {months.map(month => (
+          <DropdownMenuItem key={month} onClick={() => onMonthChange(month)} className={`text-lg p-2 ${month === selectedMonth ? 'bg-blue-50' : ''
+            }`}>
+            <span>{new Date(2024, month).toLocaleString('default', { month: 'long' })}</span>
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   )
