12c39af6d545950189f8306c9ab3d141266c1c89
Scott Gallant <scottgallant@users.noreply.github.com>
2024-04-12 11:04:53 -0300

    Update specialDates.js

--- src/data/specialDates.js
+++ src/data/specialDates.js
@@ -51,6 +51,7 @@ const specialDates = [
   { "date": "2024-03-29", "description": "Run 4K" },
     { "date": "2024-04-06", "description": "Run 3K - right knee pain (4/5) after 1.5K" },
     { "date": "2024-04-09", "description": "Strength Training " },
+      { "date": "2024-04-12", "description": "Run 4K - 35 mins, no knee pain" },
 ];
 
 export default specialDates;
