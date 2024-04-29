f04b2040fec5d7b617f7a799ace2689435343252
Scott Gallant <scottgallant@users.noreply.github.com>
2024-04-29 13:28:46 -0300

    Update specialDates.js

--- src/data/specialDates.js
+++ src/data/specialDates.js
@@ -57,6 +57,7 @@ const specialDates = [
   { "date": "2024-04-19", "description": "Strength Training" },
   { "date": "2024-04-22", "description": "Run 4K - sore ankle (outside and back) from walking in beach the day before" },
   { "date": "2024-04-23", "description": "Strength Training - 🚨 right ankle became super sore all week, not just the inside ligament but moreso the back and outside too" },
+  { "date": "2024-04-29", "description": "Tabata 20 mins" },
 ];
 
 
